282cfde6922ecf9c1b798461c57525cada3eb11c
Bensuperpc <bensuperpc@gmail.com>
2020-06-06 11:43:55 +0000

    Add files and scripts to fix issues

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # toshiba_protege_z930
-Scripts
+
+Fix issues with toshiba protege z930 and some others models.
+
+## Quick Start
+```sh
+sudo ./grub_fix.sh
+```
+
+## Credits 
+
+[malbo](Thank https://forum.ubuntu-fr.org/viewtopic.php?id=1988359) Thanks for your help !
---
+++ grub_fix.sh
@@ -0,0 +1,19 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "Please use: <script> <installation path of EFI part.>"
+    exit
+fi
+
+sudo mount /dev/$1 /mnt
+sudo mkdir /mnt/EFI/BOOT
+sudo cp /mnt/EFI/ubuntu/* /mnt/EFI/BOOT
+sudo mv /mnt/EFI/BOOT/grubx64.efi /mnt/EFI/BOOT/bootx64.efi
+echo "OK"
+
+#Thank https://forum.ubuntu-fr.org/viewtopic.php?id=1988359
